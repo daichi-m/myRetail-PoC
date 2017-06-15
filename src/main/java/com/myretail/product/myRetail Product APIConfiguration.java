400667a892a189c12c67ce999900067058edd5f5
daichi-m <frendztitan@gmail.com>
2017-06-15 07:22:05 +0530

    Initial commit

---
+++ src/main/java/com/myretail/product/myRetail Product APIConfiguration.java
@@ -0,0 +1,10 @@
+package com.myretail.product;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class myRetail Product APIConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
