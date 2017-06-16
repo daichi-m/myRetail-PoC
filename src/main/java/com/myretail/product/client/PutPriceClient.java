24a18b15cebcb55e0075dfe9b45d0e0af507012d
daichi-m <frendztitan@gmail.com>
2017-06-16 10:58:35 +0530

    Adding  test clients

---
+++ src/main/java/com/myretail/product/client/PutPriceClient.java
@@ -0,0 +1,47 @@
+package com.myretail.product.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.EnglishReasonPhraseCatalog;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.luaj.vm2.ast.Str;
+
+import java.io.IOException;
+import java.util.Locale;
+
+/**
+ * Test client to put price information
+ */
+@Slf4j
+public class PutPriceClient {
+
+    public static void main(String[] args) {
+
+        String json = "{\"name\":\"The Big Lebowski (Blu-ray)\",\"id\":\"13860428\",\"price_info\":{\"price\":21," +
+                "\"currency_code\":\"USD\"}}";
+        HttpClient client = HttpClients.createDefault();
+        HttpPut request = new HttpPut("http://localhost:8090/myRetail/v1/products/13860428");
+        StringEntity jsonEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
+        request.setEntity(jsonEntity);
+        try {
+            CloseableHttpResponse response = (CloseableHttpResponse) client.execute(request);
+            int respCode = response.getStatusLine().getStatusCode();
+            String respReason = EnglishReasonPhraseCatalog.INSTANCE.getReason(respCode, Locale.ENGLISH);
+            log.info("{} {}", respCode, respReason);
+            String respString = EntityUtils.toString(response.getEntity());
+            log.info("Response: {}", respString);
+        } catch (IOException ex) {
+            log.error("Error occurred", ex);
+        }
+
+    }
+
+}
